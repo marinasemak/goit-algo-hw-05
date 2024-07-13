b867e29be0e5940b7f048fa2ead3a814b83b5552
marinasemak <marina.semak@gmail.com>
2024-07-13 15:22:23 +0300

    add task1

---
+++ task1.py
@@ -0,0 +1,25 @@
+def caching_fibonacci():
+    cache = {}
+
+    def fibonacci(n):
+        """
+        Calculates fibonacci number
+        """
+        if n == 0:
+            return 0
+        if n == 1:
+            return 1
+        if n in cache:
+            return cache[n]
+        else:
+            cache[n] = fibonacci(n - 1) + fibonacci(n - 2)
+            return cache[n]
+
+    return fibonacci
+
+
+calculate_fibonacci = caching_fibonacci()
+
+print(calculate_fibonacci(1))
+print(calculate_fibonacci(6))
+print(calculate_fibonacci(10))

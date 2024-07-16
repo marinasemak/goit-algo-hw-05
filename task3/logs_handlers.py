f1adce0f9992773d295af25f7908843d38118785
marinasemak <marina.semak@gmail.com>
2024-07-16 21:41:21 +0300

    task3 final version

--- task3/logs_handlers.py
+++ task3/logs_handlers.py
@@ -1,39 +1,49 @@
-
 from collections import Counter
 
 
 def parse_log_line(line: str) -> dict:
+    """
+    Parse line from logs and converts it into dictionary
+    """
     components_names = ["data", "time", "level", "message"]
-    line_list = line.split(maxsplit=3)
+    line_list = line.strip().split(maxsplit=3)
     components = dict(zip(components_names, line_list))
     return components
 
 
 def load_logs(file_path: str) -> list:
-
+    """
+    Opens log file
+    """
     with open(file_path, "r", encoding="utf-8") as fh:
         parsed_logs = [parse_log_line(line) for line in fh.readlines()]
     return parsed_logs
 
 
 def filter_logs_by_level(logs: list, level: str) -> list:
+    """
+    Filter logs according to specified level
+    """
     level = level.upper()
 
     def contains_level(log, level):
         return any(level in value for value in log.values())
 
-    filtered_logs = list(filter(lambda log: contains_level(log, level), logs))
-    return filtered_logs
+    return list(filter(lambda log: contains_level(log, level), logs))
 
 
 def count_logs_by_level(logs: list) -> dict:
+    """
+    Calculates quantity of lines for each logs level
+    """
     levels = [log["level"] for log in logs if "level" in log]
-    counts = dict(Counter(levels))
-    # print(level_quantities)
-    return counts
+    return dict(Counter(levels))
 
 
 def display_log_counts(counts: dict):
+    """
+    Format and display result
+    """
     # Print the names of the columns.
     print("{:<20} {:<10}".format("Рівень логування", "Кількість"))
 
--- task3/main.py
+++ task3/main.py
@@ -1,63 +1,38 @@
-
 import sys
 
-from logs_handlers import load_logs, filter_logs_by_level, count_logs_by_level, display_log_counts
-
-
-# path_to_logs = Path("task3/logs.txt")
-
-
-# for arg in sys.argv:
-#     path_to_logs = sys.argv[1]
-#     level = sys.argv[2]
-# print(path_to_logs)
-
-# list_logs = load_logs(path_to_logs)
-# print(filter_logs_by_level(list_logs, level))
-
-# d = count_logs_by_level(list_logs)
-# display_log_counts(d)
+from logs_handlers import (
+    load_logs,
+    filter_logs_by_level,
+    count_logs_by_level,
+    display_log_counts,
+)
 
 
 def main():
+    # Check if enetered additional arguments from command line
     if len(sys.argv) > 1:
         path_to_logs = sys.argv[1]
-        logs_list = load_logs(path_to_logs)
+        try:
+            logs_list = load_logs(path_to_logs)
+        except:
+            FileNotFoundError
+            print(f"Error: File '{path_to_logs}' not found.")
+            sys.exit(1)
 
+        # Calculates and display results of lines quantity for each level
         error_counts = count_logs_by_level(logs_list)
         display_log_counts(error_counts)
+
+        # Display all lines for a level if it was specified in command line
         if len(sys.argv) > 2:
             level = sys.argv[2]
             filtered_logs = filter_logs_by_level(logs_list, level)
             print(f"Деталі логів для рівня '{level.upper()}':")
             for logs in filtered_logs:
-                print(
-                    f"{logs['data']} {logs['time']} - {logs['message']}", end="")
+                print(f"{logs['data']} {logs['time']} - {logs['message']}")
+    else:
+        print("Usage: main.py <path_to_logs> <level>")
 
 
 if __name__ == "__main__":
     main()
-
-"""Рівень логування | Кількість
------------------|----------
-INFO             | 4
-DEBUG            | 3
-ERROR            | 2
-WARNING          | 1"""
-
-# python main.py path/to/logfile.log error
-# [{'data': '2024-01-22', 'time': '09:00:45', 'level': 'ERROR', 'message': 'Database connection failed.\n'}, {'data': '2024-01-22', 'time': '11:30:15', 'level': 'ERROR', 'message': 'Backup process failed.\n'}]
-
-"""
-    Рівень логування | Кількість
------------------|----------
-INFO             | 4
-DEBUG            | 3
-ERROR            | 2
-WARNING          | 1
-
-Деталі логів для рівня 'ERROR':
-2024-01-22 09:00:45 - Database connection failed.
-2024-01-22 11:30:15 - Backup process failed.
-
-    """
